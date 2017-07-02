c456287ebc660783b4e4e3a431841cb3fbae78c2
Park MiReuk <mkcode2014@gmail.com>
2017-07-02 17:53:53 +0900

    feat: implement solve method

--- src/main/kotlin/com/rainist/ioextended/Main.kt
+++ src/main/kotlin/com/rainist/ioextended/Main.kt
@@ -2,5 +2,11 @@ package com.rainist.ioextended
 
 
 fun solve(combinedList: List<Long>): List<Long> {
-    return emptyList()
+
+    fun loop(acc: List<Long>, inputs: List<Long>): List<Long> {
+        return if (inputs.isEmpty()) acc
+        else inputs.first().let { loop(acc.plus(it), inputs.minus(it).minus(it * 4 / 3)) }
+    }
+
+    return loop(emptyList(), combinedList)
 }
